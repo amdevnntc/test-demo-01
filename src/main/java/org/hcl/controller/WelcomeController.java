49916cc9a7acf3b96dbe54291d7b52e721bbb110
amdevnntc <amdevnntc@gmail.com>
2020-04-07 10:22:15 +0000

    changes in controller

--- src/main/java/org/hcl/controller/WelcomeController.java
+++ src/main/java/org/hcl/controller/WelcomeController.java
@@ -1,13 +1,18 @@
 package org.hcl.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 	
-	
+	@GetMapping("/")
 	public String showform() {
 		return "welcome";
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello Docker";
 	}
 
 }
